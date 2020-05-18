07d49f0e8056a9ceef95e2a640385330fe470442
Atif Nabil <atif.nabil@modiscloud.net>
2020-05-18 05:52:19 +0200

    use animation in naviagtion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {fadeAnimation} from './_animations/animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [fadeAnimation]
 })
 export class AppComponent implements OnInit {
   constructor(public router: Router) {
